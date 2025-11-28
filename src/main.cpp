24e102584ed546fef3753eb9916e3c57cb0bb28c
Mitch Waghorn <m.waghorn2000@gmail.com>
2025-11-28 21:21:10 +1100

    Added a to float function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
+#include <cassert>
 #include <iostream>
+#include <q8_8.h>
+
+void q8_8_tests();
+void test_framework(float E, float R);
 
 int main() {
     std::cout << "Hello C++ development environment!" << std::endl;
+    std::cout << "Running Tests!..." << std::endl;
+    q8_8_tests();
     return 0;
+}
+
+void q8_8_tests() {
+    q8_8 test = q8_8(0x8180);
+    float result = test.toFloat();
+    float expected = -1.5f;
+    test_framework(expected, result);
+
+    test = q8_8(0x4061);
+    result = test.toFloat();
+    expected = 64.3789;
+    test_framework(expected, result);
+}
+
+void test_framework(const float E, const float R) {
+    std::cout << "R: " << R << " E: " << E;
+    if (R != E) {
+        assert(false);
+    }
+    std::cout << " ....   Passed" << std::endl;
 }
--- src/q8_8.cpp
+++ src/q8_8.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "../include/q8_8.h"
+
+#include <complex>
+
+#define SIGN_MASK (1 << 15)
+#define INTEGER_MASK  (0x007F << 8)
+#define FRACTION_MASK (0x00FF)
+
+// Default constructor
+q8_8::q8_8() {
+    this->value = 0;
+}
+
+q8_8::q8_8(const int16_t v) {
+    this->value = v;
+}
+
+float q8_8::toFloat() const {
+    const int16_t& v = this -> value;
+    const float integer = ((INTEGER_MASK & v) >> 8);
+    const uint16_t fraction = ((FRACTION_MASK & v));
+    const uint16_t sign = ((SIGN_MASK & v)) >> 15;
+
+    uint16_t mask = 0x80;
+    float fraction_sum = 0.0;
+    for (int i = 8; i > 0; i--) {
+        if (mask & fraction) {
+            fraction_sum += static_cast<float_t> (mask) / 256.0f;
+        }
+        mask >>= 1;
+    }
+
+    float result = integer + fraction_sum;
+    result = sign ? -result : result;
+    return result;
+}
+
+
+
